fbfc6c6d4b229bf119a76be1463c1d787bc6dd6c
Kevin Mändmets <mandmetskevin@gmail.com>
2024-07-16 12:04:52 +0300

    remove redundant comments

--- library/views.py
+++ library/views.py
@@ -3,8 +3,6 @@ from .models import Book, Author
 from . import serializers
 
 
-# TODO create a filter backend that filters the books by the books publishing date
-# TODO or implement the filtering in the viewset directly in the get_queryset method
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     # filter_backends = []
